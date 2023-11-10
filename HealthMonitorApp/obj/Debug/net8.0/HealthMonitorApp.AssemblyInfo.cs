48549e6c3b5a729bfd50fbc951e3968408cf7013
df.muhammed.bel <df.muhammed.bel@a101.com.tr>
2023-11-10 15:39:58 +0300

    added to bucket

--- HealthMonitorApp/obj/Debug/net8.0/HealthMonitorApp.AssemblyInfo.cs
+++ HealthMonitorApp/obj/Debug/net8.0/HealthMonitorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthMonitorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+841941b0312f1526c8f8dcedb670352c7b73fe44")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthMonitorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthMonitorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
